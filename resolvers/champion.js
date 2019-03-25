9f3fec41b4e2243cc232bd2f5dcffd8d4ded44d6
Polones <augusto.cheme@gmail.com>
2019-03-25 18:52:39 -0300

    Fix getChampionByName

--- resolvers/champion.js
+++ resolvers/champion.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch')
 module.exports = {
 
     getChampionByName: async function (name) {
-        fetch(`https://ddragon.leagueoflegends.com/cdn/8.24.1/data/pt_BR/champion/${args.name}.json`).then(res => res.json()).then(json => json.data.name)
+        return fetch(`https://ddragon.leagueoflegends.com/cdn/8.24.1/data/pt_BR/champion/${name}.json`).then(res => res.json()).then(json => json.data[name])
     }
 
 }
